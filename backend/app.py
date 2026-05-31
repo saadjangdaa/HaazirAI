684efcafa77098c468c12cbc7c57f7c3d7d7d7b7
Saad Jangda <73118409+saadjangdaa@users.noreply.github.com>
2026-05-31 05:17:23 +0500

    update cors issue

--- backend/app.py
+++ backend/app.py
@@ -53,7 +53,7 @@ app.state.instance_id = APP_INSTANCE_ID
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
