e5ae50825c7cacbc04ba29f2fe90fa94b0884838
Naba Yousuf <nabay0519@gmail.com>
2026-06-02 02:32:06 +0500

    version update and error fix

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,6 +1,14 @@
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+// Avoid Metro crashes when temp Gradle / autolinking folders disappear mid-watch.
+config.resolver.blockList = [
+  ...(config.resolver.blockList ?? []),
+  /[/\\]\.gradle[/\\].*/,
+  /[/\\]node_modules[/\\]\.expo-modules-autolinking-[^/\\]+[/\\].*/,
+];
 
 module.exports = config;
